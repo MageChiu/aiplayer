b8ff810ec14d10284d503b2ff7f8594ed79ff0e3
zhaopeng.charles <zhaopeng.charles@bytedance.com>
2026-04-16 11:24:21 +0800

    bugfix win

--- build/macos/CMakeFiles/aiplayer_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/aiplayer_autogen"
-  "aiplayer_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/aiplayer_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/macos/_deps/whispercpp-build/ggml/src/CMakeFiles/ggml_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/ggml_autogen"
-  "ggml_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/ggml_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
